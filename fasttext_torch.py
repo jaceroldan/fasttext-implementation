1eccee227cecff18224ba4098c8a36b50865db93
Jace Roldan <jaceroldan16@gmail.com>
2024-09-21 21:32:18 +0800

    Rename to fasttext_torch

--- fasttext_torch.py
+++ fasttext_torch.py
@@ -1,9 +1,19 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class SkipGramModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(SkipGramModel, self).__init__()
+        self.target_embeddings = nn.Embedding
 
 class FastText:
     def __init__(self, vocab_size, embedding_dim, ngram_range=3):
         self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.ngram_range = ngram_range
 
     def generate_ngrams(self, word, override_n=None):
         if override_n:

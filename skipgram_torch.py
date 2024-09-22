b65ff01d5657c3c0e356fe1778e2ea407da669f3
Jace Roldan <jaceroldan16@gmail.com>
2024-09-22 09:54:01 +0800

    Update training for skipgram model
    Added a Vocabulary class to properly manage the amount of text from coleridge. And
    added a class for efficient loading (from Pytorch).
    Next: I will implement a version that uses FastText's character-level n-grams.

--- skipgram_torch.py
+++ skipgram_torch.py
@@ -1,4 +1,6 @@
+import json
 import os
+import re
 
 import pandas as pd
 
@@ -6,11 +8,67 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+
+from collections import Counter
+
+
+class Vocabulary:
+    def __init__(self, min_freq=1):
+        self.word2idx = {'<UNK>': 0}  # Add a special token for unknown words
+        self.idx2word = {0: '<UNK>'}
+        self.min_freq = min_freq
+    
+    def build_vocab(self, texts):
+        word_counts = Counter(re.findall(r'\w+', ' '.join(texts).lower()))
+
+        idx = 1  # Start indices from 1 since 0 is reserved for <UNK>
+        for word, count in word_counts.items():
+            if count >= self.min_freq:
+                self.word2idx[word] = idx
+                self.idx2word[idx] = word
+                idx += 1
+
+    def encode(self, text):
+        # Return word index or 0 if word is not found in vocab
+        # TODO: There must be a smarter way to deal with words not in vocabulary.
+        # Using the <UNK> token above gets me by for now.
+        return [self.word2idx.get(word, 0) for word in re.findall(r'\w+', text.lower())]
+    
+    def vocab_size(self):
+        return len(self.word2idx)
 
 
 class CustomColeridgeDataset(Dataset):
+    def __init__(self, csv_file, json_dir, vocab, window_size=2, n_samples=1000, random_state=42):
+        self.train = pd.read_csv(csv_file)
+        self.train_items = self.train.sample(n=n_samples, random_state=random_state)
+        self.json_dir = json_dir
+        self.vocab = vocab
+        self.window_size = window_size
 
+    def __len__(self):
+        return len(self.train_items)
+    
+    def __getitem__(self, idx):
+        train_id = self.train_items.iloc[idx]['Id']
+        curr_path = os.path.join(self.json_dir, train_id + '.json')
+
+        with open(curr_path, 'r') as file:
+            curr_json = json.load(file)
+
+        text = ''.join([cj['text'] for cj in curr_json])
+        word_indices = self.vocab.encode(text)
+
+        # generate center-context pairs using the window size
+        center_context_pairs = []
+        for i, center_word_idx in enumerate(word_indices):
+            for j in range(max(0, i - self.window_size), min(len(word_indices), i + self.window_size + 1)):
+                if i != j:
+                    context_word_idx = word_indices[j]
+                    center_context_pairs.append((center_word_idx, context_word_idx))
+
+        return center_context_pairs
 
 
 class SkipGramModel(nn.Module):
@@ -47,41 +105,47 @@ def generate_negative_samples(batch_size, vocab_size, num_neg_samples, true_cont
 
 
 if __name__ == '__main__':
-    vocab_size = 10_000
+    vocab = Vocabulary(min_freq=5)
+
+    train = pd.read_csv('./datasets/train.csv')
+    train_items = train.sample(n=1000, random_state=42)
+    texts = []
+
+    for i in range(len(train_items)):
+        curr_path = os.path.join(
+            os.getcwd(), 'datasets', 'train', train_items.iloc[i]['Id'] + '.json')
+
+        with open(curr_path, 'r') as file:
+            curr_json = json.load(file)
+            texts.append(''.join([cj['text'] for cj in curr_json]))
+
+    vocab.build_vocab(texts)
+    
+    dataset = CustomColeridgeDataset(csv_file='./datasets/train.csv', json_dir='./datasets/train/', vocab=vocab)
+
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=lambda x: [item for sublist in x for item in sublist])
+
+    vocab_size = vocab.vocab_size()
     embedding_dim = 100
-    batch_size = 64
-    num_neg_samples = 5
-    learning_rate = 0.01
-    num_epochs = 10
+    model = SkipGramModel(vocab_size, embedding_dim)
 
-    for i in range(num_epochs):
-        model = SkipGramModel(vocab_size, embedding_dim)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    for batch in dataloader:
+        center_word_idxs, context_word_idxs = zip(*batch)
+        center_word_idxs = torch.tensor(center_word_idxs, dtype=torch.long)
+        context_word_idxs = torch.tensor(context_word_idxs, dtype=torch.long)
 
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-        # TODO: Extract this from the dataset of 1000 files
-        center_word_idxs = torch.randint(0, vocab_size, (batch_size,))
-        context_word_idxs = torch.randint(0, vocab_size, (batch_size,))
-
-        neg_samples = generate_negative_samples(batch_size, vocab_size, num_neg_samples, context_word_idxs)
-
-        true_labels = torch.ones(batch_size)
-        neg_labels = torch.zeros(batch_size, num_neg_samples)
+        if center_word_idxs.max().item() >= vocab_size or context_word_idxs.max().item() >= vocab_size:
+            print(f"Error: found out-of-bounds index. Max center: {center_word_idxs.max()}, Max context: {context_word_idxs.max()}")
+            continue
 
         optimizer.zero_grad()
-        positive_scores = model(center_word_idxs, context_word_idxs)
+        scores = model(center_word_idxs, context_word_idxs)
 
-        negative_scores = torch.zeros(batch_size, num_neg_samples)
-        for i in range(num_neg_samples):
-            negative_scores[:, i] = model(center_word_idxs, neg_samples[:, i])
-        
-
-        positive_loss = skipgram_loss(positive_scores, true_labels)
-        negative_loss = skipgram_loss(negative_scores.view(-1), neg_labels.view(-1))
-
-        total_loss = positive_loss + negative_loss
-        total_loss.backward()
-
+        true_labels = torch.ones_like(scores)
+        loss = skipgram_loss(scores, true_labels)
+        loss.backward()
         optimizer.step()
 
-        print(f"Loss: {total_loss.item()}")
+        # Benchmark loss scores at: 0.02
+        print(f'Loss: {loss.item()}')
